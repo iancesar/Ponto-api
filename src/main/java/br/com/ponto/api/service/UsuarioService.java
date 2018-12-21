478d4e9c00a2f311ccd3461000d630c1b1363ae1
ian <ian@spdata-web-ian>
2018-12-21 09:36:14 -0200

    s

--- src/main/java/br/com/ponto/api/service/UsuarioService.java
+++ src/main/java/br/com/ponto/api/service/UsuarioService.java
@@ -1,45 +1,50 @@
 package br.com.ponto.api.service;
 
-import java.util.List;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 
 import br.com.ponto.api.dao.UsuarioDAO;
+import br.com.ponto.api.model.Expediente;
 import br.com.ponto.api.model.Usuario;
 
-@Path("/api/user")
-@Produces({"application/json"})
+@Stateless
 public class UsuarioService
 {
 
 	@Inject
 	private UsuarioDAO usuarioDao;
 
-	@Path("/create")
-	@POST
 	public Usuario create(final Usuario u)
 	{
+
+		u.setExpedienteList(new ArrayList<>());
+		final DayOfWeek[] dias = DayOfWeek.values();
+
+		for(final DayOfWeek dia : dias)
+		{
+			final Expediente e = new Expediente();
+			e.setDia(dia.getValue());
+			e.setEntrada(createDate(8, 0, 0));
+			e.setSaidaAlmoco(createDate(12, 0, 0));
+			e.setRetornoAlmoco(createDate(13, 30, 0));
+			e.setSaida(createDate(18, 0, 0));
+			e.setHabilitado(false);
+			e.setUsuario(u);
+			u.getExpedienteList().add(e);
+		}
+
 		return usuarioDao.create(u);
 	}
 
-	@Path("/list")
-	@GET
-	public List<Usuario> list()
+	private Date createDate(final int hours, final int minus, final int seconds)
 	{
-		return usuarioDao.list();
-	}
-
-	@Path("/delete/{id}")
-	@DELETE
-	public void remove(@PathParam("id") final Integer id)
-	{
-		usuarioDao.remove(id);
+		return Date.from(LocalDateTime.now().withHour(hours).withMinute(minus).withSecond(seconds).atZone(ZoneId.systemDefault()).toInstant());
 	}
 
 }
